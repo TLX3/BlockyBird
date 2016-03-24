58f7fbe193a14a132f935f69812885ca07195aa7
TLX3 <tdl2116@columbia.edu>
2016-03-23 22:31:13 -0400

    Add score and handle collisions

--- main.js
+++ main.js
@@ -17,20 +17,31 @@ var mainState = {
 
       this.pipes = game.add.group();
       this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+      this.score = 0;
+      this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
     },
 
     update: function() {
       if(this.bird.y < 0 || this.bird.y > 490) {
         this.restartGame();
       }
+      if(this.bird.angle < 20) {
+        this.bird.angle += 1;
+      }
+      game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this)
     },
 
     jump: function() {
         this.bird.body.velocity.y = -350;
+        var animation = game.add.tween(this.bird);
+        this.bird.anchor.setTo(-0.2, 0.5);
+        animation.to({angle: -20}, 100);
+        animation.start();
     },
 
     restartGame: function() {
-      game.starte.start('main');
+      game.state.start('main');
     },
 
     addOnePipe: function(x, y) {
@@ -49,6 +60,8 @@ var mainState = {
           this.addOnePipe(400, i*60 +10);
         }
       }
+      this.score += 1;
+      this.labelScore.text = this.score;
     }
 
 };
